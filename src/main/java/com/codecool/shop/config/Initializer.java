add752df82ddc69158806aaa89c1175865c167a9
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-08-28 11:41:31 +0200

    dropdown menu with all product category

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -19,7 +19,6 @@ public class Initializer implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        // r
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
@@ -33,9 +32,14 @@ public class Initializer implements ServletContextListener {
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
+        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "These are portable laptops.");
+        productCategoryDataStore.add(laptop);
+        ProductCategory mobil = new ProductCategory("Mobil", "Hardware", "These are mobile phones.");
+        productCategoryDataStore.add(mobil);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+//        productDataStore.add(new Product("Apple Iphone", 500.0f, "USD", "Fantastic price.The best phone ever.", mobil, amazon));
+        productDataStore.add(new Product("Apple Macbook Pro", 5000.0f, "USD", "Very high price. Best portable laptop ever.", laptop, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
     }
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -34,6 +34,7 @@ public class ProductController extends HttpServlet {
 //        context.setVariables(params);
         context.setVariable("recipient", "World");
         context.setVariable("category", productCategoryDataStore.find(1));
+        context.setVariable("allProductCategory", productCategoryDataStore.getAll());
         context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         engine.process("product/index.html", context, resp.getWriter());
     }
