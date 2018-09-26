423ad5b4d4927d90aa57b774cebef580ac6f7b3e
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-09-26 11:53:21 +0200

    new user's password already hashed

---
+++ src/main/java/com/codecool/shop/utilities/Bcrypt.java
@@ -0,0 +1,17 @@
+package com.codecool.shop.utilities;
+
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class Bcrypt {
+
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean verifyPassword(String candidate_password, String hashedPassword) {
+        return BCrypt.checkpw(candidate_password, hashedPassword);
+
+    }
+
+}
