1d060b830828cb6e20585b619fabf8aead3f9e8e
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-09-26 09:59:18 +0200

    database config file updated with new name customer -> user

--- src/main/resources/databasefiles/webshop_db_schema.sql
+++ src/main/resources/databasefiles/webshop_db_schema.sql
@@ -1,47 +1,47 @@
-CREATE TABLE customer (
-   id serial  NOT NULL,
-   name varchar(255)  NOT NULL,
-   email varchar(255)  NOT NULL,
-   password varchar(100)  NOT NULL,
-   CONSTRAINT customer_pk PRIMARY KEY (id)
+CREATE TABLE "user" (
+  id serial  NOT NULL,
+  name varchar(255)  NOT NULL,
+  email varchar(255)  NOT NULL,
+  password varchar(100)  NOT NULL,
+  CONSTRAINT user_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE "order" (
-   id serial  NOT NULL,
-   custumer_id int  NOT NULL,
-   CONSTRAINT order_pk PRIMARY KEY (id)
+  id serial  NOT NULL,
+  user_id int  NOT NULL,
+  CONSTRAINT order_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE purchase_item (
-   id serial  NOT NULL,
-   order_id int  NOT NULL,
-   product_id int  NOT NULL,
-   number_of_items int  NOT NULL,
-   CONSTRAINT purchase_item_pk PRIMARY KEY (id)
+  id serial  NOT NULL,
+  order_id int  NOT NULL,
+  product_id int  NOT NULL,
+  number_of_items int  NOT NULL,
+  CONSTRAINT purchase_item_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE product (
-   id serial  NOT NULL,
-   name varchar(255)  NOT NULL,
-   default_price numeric  NOT NULL,
-   currency varchar(255)  NOT NULL,
-   description varchar(255)  NOT NULL,
-   product_category_id int  NOT NULL,
-   supplier_id int  NOT NULL,
-   CONSTRAINT product_pk PRIMARY KEY (id)
+  id serial  NOT NULL,
+  name varchar(255)  NOT NULL,
+  default_price numeric  NOT NULL,
+  currency varchar(255)  NOT NULL,
+  description varchar(255)  NOT NULL,
+  product_category_id int  NOT NULL,
+  supplier_id int  NOT NULL,
+  CONSTRAINT product_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE product_category (
-   id serial  NOT NULL,
-   name varchar(255)  NOT NULL,
-   department varchar(255)  NOT NULL,
-   description varchar(255)  NOT NULL,
-   CONSTRAINT product_category_pk PRIMARY KEY (id)
+  id serial  NOT NULL,
+  name varchar(255)  NOT NULL,
+  department varchar(255)  NOT NULL,
+  description varchar(255)  NOT NULL,
+  CONSTRAINT product_category_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE supplier (
-   id serial  NOT NULL,
-   name varchar(255)  NOT NULL,
-   description varchar(255)  NOT NULL,
-   CONSTRAINT supplier_pk PRIMARY KEY (id)
+  id serial  NOT NULL,
+  name varchar(255)  NOT NULL,
+  description varchar(255)  NOT NULL,
+  CONSTRAINT supplier_pk PRIMARY KEY (id)
 );
