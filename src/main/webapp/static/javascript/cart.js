64b0f6d28f02e049b70ab020653ac6524c9dd530
ferencDobi <ferencdbi@gmail.com>
2018-10-10 13:13:21 +0200

    totals adjustment works on frontend

--- src/main/webapp/static/javascript/cart.js
+++ src/main/webapp/static/javascript/cart.js
@@ -1,10 +1,30 @@
 $(function() {
-   $(".more").click(() => {
-       let count = $(this.activeElement).closest("tr").find(".count");
-       count.text(+count.text() + 1);
-   });
-    $(".less").click(() => {
-        let count = $(this.activeElement).closest("tr").find(".count");
-        count.text(Math.max(+count.text() - 1, 0));
+    $(".more").click(() => {
+        let [ $row, $count ] = locate(this.activeElement);
+        let oldCount = +$count.text();
+        $count.text(oldCount + 1);
+        updatePrice($row, oldCount + 1);
     });
+
+    $(".less").click(() => {
+        let [ $row, $count ] = locate(this.activeElement);
+        let oldCount = +$count.text();
+        if (oldCount === 0) return;
+        $count.text(oldCount - 1);
+        updatePrice($row, oldCount - 1, true);
+    });
+
+    const updatePrice = ($row, newCount, subtract=false) => {
+        let [ price, currency ] = $row.find(".price").text().split(" ");
+        $row.find(".total").text(`${(+price * newCount).toFixed(1)} ${currency}`);
+        let $grandTotal = $(".grand-total");
+        let newGrandTotal = +$grandTotal.text() + (subtract ? -price : +price);
+        $grandTotal.text(newGrandTotal.toFixed(1));
+    };
+
+    const locate = (element) => {
+        let $row = $(element).closest("tr");
+        let $count = $row.find(".count");
+        return [ $row, $count ];
+    }
 });
